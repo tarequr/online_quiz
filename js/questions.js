d20bf337c88f0a4cb6ffce49c07fc8c0c858f2a6
tarequr <tarequrr8@gmail.com>
2023-03-26 11:08:18 +0600

    questions created

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        number: 1,
+        question: 'What is Laravel?',
+        answer: 'A web framework',
+        options: [
+            'A programming language',
+            'A web framework',
+            'A database management system',
+            'A version control system'
+        ] 
+    },
+    {
+        number: 2,
+        question: 'Who created Laravel?',
+        answer: 'Taylor Otwell',
+        options: [
+            'Taylor Otwell',
+            'Larry Wall',
+            'Rasmus Lerdorf',
+            'Guido van Rossum'
+        ] 
+    },
+    {
+        number: 3,
+        question: 'Which programming language is used in Laravel?',
+        answer: 'PHP',
+        options: [
+            'Python',
+            'Ruby',
+            'PHP',
+            'JavaScript'
+        ] 
+    },
+    {
+        number: 4,
+        question: 'What is Eloquent in Laravel?',
+        answer: 'A database ORM',
+        options: [
+            'A template engine',
+            'A database ORM',
+            'A routing system',
+            'A testing framework'
+        ] 
+    },
+    {
+        number: 5,
+        question: "What is the name of Laravel's built-in testing tool?",
+        answer: 'PHPUnit',
+        options: [
+            'PHPUnit',
+            'Codeception',
+            'Behat',
+            'Laravel Dusk'
+        ] 
+    }
+]
