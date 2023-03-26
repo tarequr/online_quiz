841179f59e90a10dcbf14663a18220a8bd476c10
tarequr <tarequrr8@gmail.com>
2023-03-26 10:47:17 +0600

    go to questions page from rules page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 const firstBtn = document.querySelector(".btn button");
+
 const rulesBox = document.querySelector(".rulesBox");
+const exitButton = document.querySelector(".btn2 .exitBtn");
+
+const continueButton = document.querySelector(".btn2 .continueBtn");
+const questions = document.querySelector(".questions");
+
+
 
 firstBtn.onclick = () => {
     rulesBox.classList.add("activeInfo");
+}
+
+exitButton.onclick = () => {
+    rulesBox.classList.remove("activeInfo");
+}
+
+continueButton.onclick = () => {
+    rulesBox.classList.remove("activeInfo");
+    questions.classList.add("activeQuiz");
 }
